0374d2248373b546c83ba3ce4ce44486a3675b65
sahilpatel18 <sahil.41801@gmail.com>
2020-10-31 17:35:33 -0400

    mapping recommending videos and allowing user to click

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,12 +1,23 @@
-import React from 'react'
-import {Grid, Paper, Typography} from '@material-ui/core'
+import React from "react";
+import { Grid, Paper, Typography } from "@material-ui/core";
 
-const VideoItem = () => {
-    return (
-        <div>
-            vidit
-        </div>
-    )
-}
-
-export default VideoItem
+const VideoItem = ({ video, onVideoSelect }) => {
+  return (
+    <Grid item xs={12}>
+      <Paper
+        style={{ display: "flex", alignItems: "center", cursor: "pointer" }}
+        onClick={() => onVideoSelect(video)}
+      >
+        <img
+          style={{ marginRight: "20px" }}
+          alt="thumnail"
+          src={video.snippet.thumbnails.medium.url}
+        />
+        <Typography variant="subtitle1">
+          <b>{video.snippet.title}</b>
+        </Typography>
+      </Paper>
+    </Grid>
+  );
+};
+export default VideoItem;
